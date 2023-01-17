41a6c216444f00051612e4be1932e540b3e8700d
chris <104324755+cjurgens17@users.noreply.github.com>
2023-01-17 13:42:04 -0500

    Added @MappedSuperClass to Base Entity for id properties. closes #35.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     //In Hibernate box types can be null and primitives cant, thats why using Long box-type
+
+    @Id
+    @GeneratedValue(strategy = GenerationType. IDENTITY)
     private Long id;
 
     public Long getId() {
