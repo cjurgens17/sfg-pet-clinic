57d33f79d12a19cdcd01d26105e5f0135bb99aa5
chris <104324755+cjurgens17@users.noreply.github.com>
2023-01-17 14:02:32 -0500

    mapped vets and specialities. closes #37.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
